76f8529df0ef1b0105f74a8c0854b2a0a5121f76
Dorian Aldair Oliveros 0909-17-8081 <aldair_oliveros09@hotmail.es>
2021-09-10 23:16:00 -0600

    consumiendo API de Marvel

--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -1,41 +1,22 @@
+import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 
-import { Item } from './item'
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class ItemService {
-  private items = new Array<Item>(
-    { id: 1, name: 'Ter Stegen', role: 'Goalkeeper' },
-    { id: 3, name: 'Piqué', role: 'Defender' },
-    { id: 4, name: 'I. Rakitic', role: 'Midfielder' },
-    { id: 5, name: 'Sergio', role: 'Midfielder' },
-    { id: 6, name: 'Denis Suárez', role: 'Midfielder' },
-    { id: 7, name: 'Arda', role: 'Midfielder' },
-    { id: 8, name: 'A. Iniesta', role: 'Midfielder' },
-    { id: 9, name: 'Suárez', role: 'Forward' },
-    { id: 10, name: 'Messi', role: 'Forward' },
-    { id: 11, name: 'Neymar', role: 'Forward' },
-    { id: 12, name: 'Rafinha', role: 'Midfielder' },
-    { id: 13, name: 'Cillessen', role: 'Goalkeeper' },
-    { id: 14, name: 'Mascherano', role: 'Defender' },
-    { id: 17, name: 'Paco Alcácer', role: 'Forward' },
-    { id: 18, name: 'Jordi Alba', role: 'Defender' },
-    { id: 19, name: 'Digne', role: 'Defender' },
-    { id: 20, name: 'Sergi Roberto', role: 'Midfielder' },
-    { id: 21, name: 'André Gomes', role: 'Midfielder' },
-    { id: 22, name: 'Aleix Vidal', role: 'Midfielder' },
-    { id: 23, name: 'Umtiti', role: 'Defender' },
-    { id: 24, name: 'Mathieu', role: 'Defender' },
-    { id: 25, name: 'Masip', role: 'Goalkeeper' }
-  )
+  constructor(private http: HttpClient) {}
 
-  getItems(): Array<Item> {
-    return this.items
+  
+  getItems(){
+    return this.http.get("https://gateway.marvel.com:443/v1/public/comics?ts=20&apikey=c541a5963b092c5c88b066e48c32dd0e&hash=91f061f30ae9925d56ffc230f6a16127");
+    
   }
 
-  getItem(id: number): Item {
-    return this.items.filter((item) => item.id === id)[0]
+  obtenerBusqueda(titulo){
+    return this.http.get('https://gateway.marvel.com:443/v1/public/comics?ts=20&apikey=c541a5963b092c5c88b066e48c32dd0e&hash=91f061f30ae9925d56ffc230f6a16127&title='+titulo)
   }
+
 }
--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,18 +1,47 @@
 import { Component, OnInit } from '@angular/core'
+import { SearchBar } from '@nativescript/core';
 
-import { Item } from './item'
 import { ItemService } from './item.service'
 
 @Component({
   selector: 'ns-items',
-  templateUrl: './items.component.html',
+  templateUrl: './items.component.html'
 })
 export class ItemsComponent implements OnInit {
-  items: Array<Item>
+  items: Object;
+  busqueda: String;
 
-  constructor(private itemService: ItemService) {}
+  constructor(private itemService: ItemService) { }
+
+  buscarTitulo(args): void{
+    let searchBar = <SearchBar>args.object;
+    console.dir(searchBar.text);
+    this.busqueda = searchBar.text;
+  }
+
 
   ngOnInit(): void {
-    this.items = this.itemService.getItems()
+
+    if(this.busqueda == null){
+      this.itemService.getItems().subscribe(
+        response => {
+          this.items = response.data.results;
+        },
+        error => console.log(error)
+      );
+    }
+    /*else{
+      this.itemService.obtenerBusqueda(this.busqueda).subscribe(
+        comic =>{
+          this.items = comic.data.results;
+          //console.dir(comic.data.results);
+        },
+        error => console.log(error)
+      );
+    }*/
+
+
   }
+
+
 }
